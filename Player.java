853dc708dbf97e95c4b5265ef12b99836facaf75
Kyle Garcia MacbookAir <kkpgarcia@gmail.com>
2017-05-03 19:57:27 +0800

    - PhysicsEngine.java : removed comments + Engine.java : added removeObject() + GameEngine.java : added removeObject() + Player.java : added remove test control + RenderEngine.java : added removeObject() + Screen.java : added removeDrawingComponents()

--- Player.java
+++ Player.java
@@ -11,6 +11,9 @@ public class Player extends GameObject {
     private InputAction upArrow = new InputAction("up");
     private InputAction downArrow = new InputAction("down");
     private InputAction spaceBar = new InputAction("space", InputAction.ON_PRESS);
+    private InputAction backSpace = new InputAction("backSpace", InputAction.ON_PRESS);
+
+    private Player p2;
 
     public Player() {
         super();
@@ -31,6 +34,7 @@ public class Player extends GameObject {
         input.mapToKey(upArrow, KeyEvent.VK_UP);
         input.mapToKey(downArrow, KeyEvent.VK_DOWN);
         input.mapToKey(spaceBar, KeyEvent.VK_SPACE);
+        input.mapToKey(backSpace, KeyEvent.VK_BACK_SPACE);
     }
 
     public void update() {
@@ -55,8 +59,13 @@ public class Player extends GameObject {
 
         if(spaceBar.isPressed()) {
             System.out.println("Creating new Object!");
-            Player player2 = new Player();
-            GameEngine.instance.addObject(player2);
+            p2 = new Player();
+            GameEngine.instance.addObject(p2);
+        }
+
+        if(backSpace.isPressed()) {
+            System.out.println("Removing object");
+            GameEngine.instance.removeObject(p2);
         }
     }
 }
