17efd3b94affcec21d60ecbffaecd034b4a2cf72
Kyle Garcia MacbookAir <kkpgarcia@gmail.com>
2017-05-14 02:32:06 +0800

    added java docs to network library

--- SKEngine/Network/NetworkClientConnection.java
+++ SKEngine/Network/NetworkClientConnection.java
@@ -3,17 +3,35 @@ package SKEngine.Network;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
+/**
+ * <h2>Network Client Connection</h2>
+ * A server node connecting the client and the server.
+ * <p>
+ * 
+ * @author  Kyle Kristopher P. Garcia
+ * @since   2017-09-05
+ * */
 public class NetworkClientConnection {
     public String id;
     private NetworkDispatcher dispatcher;
     private NetworkListener listener;
 
+    /**
+     * Constructs a connection between a client, and the server.
+     * @param String connection id
+     * @param DataInputStream input stream from socket
+     * @param DataOutputStream output stream from socket
+     * */
     public NetworkClientConnection(String id, DataInputStream input, DataOutputStream output) {
         this.id = id;
         listener = new NetworkListener(input);
         dispatcher = new NetworkDispatcher(output);
     }
 
+    /**
+     * Initializes this connection, and runs the threads for the
+     * listeners, and the dispatchers.
+     * */
     public void initialize() {
         Thread dispatcherThread = new Thread(dispatcher);
         Thread listenerThread = new Thread(listener);
@@ -21,10 +39,18 @@ public class NetworkClientConnection {
         dispatcherThread.start();
     }
 
+    /**
+     * Sends a task to the dispatcher.
+     * @param NetworkTask task
+     * */
     public void dispatch(NetworkTask task) {
         dispatcher.dispatch(task);
     }
 
+    /**
+     * Retrieve a task from the listener.
+     * return NetworkTask task
+     * */
     public NetworkTask listen() {
         NetworkTask task = listener.listen();
         if(task != null)
